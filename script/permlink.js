0a6406a5102bb01efe62d743cd225b1a886071f7
Loki Astari <Loki.Astari@gmail.com>
2021-04-22 12:47:52 -0700

    Added first version of real review

--- script/permlink.js
+++ script/permlink.js
@@ -1,6 +1,6 @@
 
 module.exports = function(source, layout) {
     var link = '/' + source.replace(/\.md$/, '.html');
-    link = link.toLowerCase().replace(/^\//, '').replace(/\//g, '-').replace(/ /g, '-').replace(/-+/g, '-');
+    link = link.toLowerCase().replace(/ /g, '-').replace(/-+/g, '-');
     return link;
 }
